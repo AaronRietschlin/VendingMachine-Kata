7aadbcb49087ce9db79764423e8704503cb80211
Aaron Rietschlin <aaron.rietschlin@duethealth.com>
2017-06-01 16:07:29 -0400

    Added method for checking if price is proper.

--- model/src/main/java/com/ariets/kata/model/MoneyValidator.java
+++ model/src/main/java/com/ariets/kata/model/MoneyValidator.java
@@ -1,8 +1,14 @@
 package com.ariets.kata.model;
 
+import static com.ariets.kata.model.Coin.PENNY;
+
 public class MoneyValidator {
 
     public boolean isValid(Coin coin) {
+        return coin != PENNY;
+    }
+
+    public boolean isCorrectPayment(double currentPayment, Product product) {
         return false;
     }
 
--- model/src/test/java/com/ariets/kata/model/MoneyValidatorTest.java
+++ model/src/test/java/com/ariets/kata/model/MoneyValidatorTest.java
@@ -7,9 +7,15 @@ import static com.ariets.kata.model.Coin.DIME;
 import static com.ariets.kata.model.Coin.NICKEL;
 import static com.ariets.kata.model.Coin.PENNY;
 import static com.ariets.kata.model.Coin.QUARTER;
+import static com.ariets.kata.model.Product.CANDY;
+import static com.ariets.kata.model.Product.CHIPS;
+import static com.ariets.kata.model.Product.COLA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MoneyValidatorTest {
+
+    private static final double PRICE_LARGE = 1.70;
+    private static final double PRICE_SMALL = 0.10;
 
     private MoneyValidator moneyValidator;
 
@@ -38,5 +44,49 @@ public class MoneyValidatorTest {
         assertThat(moneyValidator.isValid(PENNY)).isFalse();
     }
 
+    @Test
+    public void isCorrectPaymentForChipsReturnsTrueForExactAmount() {
+        assertThat(moneyValidator.isCorrectPayment(CHIPS.getPrice(), CHIPS)).isTrue();
+    }
+
+    @Test
+    public void isCorrectPaymentForChipsReturnsTrueForGreaterAmount() {
+        assertThat(moneyValidator.isCorrectPayment(PRICE_LARGE, CHIPS)).isTrue();
+    }
+
+    @Test
+    public void isCorrectPaymentForChipsReturnsFalseForSmallerAmount() {
+        assertThat(moneyValidator.isCorrectPayment(PRICE_SMALL, CHIPS)).isFalse();
+    }
+
+    @Test
+    public void isCorrectPaymentForColaReturnsTrueForExactAmount() {
+        assertThat(moneyValidator.isCorrectPayment(COLA.getPrice(), COLA)).isTrue();
+    }
+
+    @Test
+    public void isCorrectPaymentForColaReturnsTrueForGreaterAmount() {
+        assertThat(moneyValidator.isCorrectPayment(PRICE_LARGE, COLA)).isTrue();
+    }
+
+    @Test
+    public void isCorrectPaymentForColaReturnsFalseForSmallerAmount() {
+        assertThat(moneyValidator.isCorrectPayment(PRICE_SMALL, COLA)).isFalse();
+    }
+
+    @Test
+    public void isCorrectPaymentForCandyReturnsTrueForExactAmount() {
+        assertThat(moneyValidator.isCorrectPayment(CANDY.getPrice(), CANDY)).isTrue();
+    }
+
+    @Test
+    public void isCorrectPaymentForCandyReturnsTrueForGreaterAmount() {
+        assertThat(moneyValidator.isCorrectPayment(PRICE_LARGE, CANDY)).isTrue();
+    }
+
+    @Test
+    public void isCorrectPaymentForCandyReturnsFalseForSmallerAmount() {
+        assertThat(moneyValidator.isCorrectPayment(PRICE_SMALL, CANDY)).isFalse();
+    }
 
 }
