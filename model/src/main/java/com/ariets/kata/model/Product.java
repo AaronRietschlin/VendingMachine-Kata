31198daf56a0f6f86721743b7beb83034081919e
Aaron Rietschlin <aaron.rietschlin@duethealth.com>
2017-06-01 15:43:01 -0400

    Setup the model class as well as setting up dependencies for tests

---
+++ model/src/main/java/com/ariets/kata/model/Product.java
@@ -0,0 +1,18 @@
+package com.ariets.kata.model;
+
+public enum Product {
+
+    COLA(1.00),
+    CHIPS(0.50),
+    CANDY(0.65);
+
+    private final double price;
+
+    Product(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
