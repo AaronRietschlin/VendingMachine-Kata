7f97f0609076bc05b47bf57a1bbaf16f246f3c5c
Aaron Rietschlin <aaron.rietschlin@duethealth.com>
2017-06-02 13:15:29 -0400

    Set up clickable state for CoinView.

--- app/src/main/java/com/ariets/kata/ui/MainActivity.java
+++ app/src/main/java/com/ariets/kata/ui/MainActivity.java
@@ -3,11 +3,15 @@ package com.ariets.kata.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
 import com.ariets.kata.R;
+import com.ariets.kata.ui.widget.CoinView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setSupportActionBar(toolbar);
+    }
+
+    @OnClick({
+            R.id.coin_view_quarter,
+            R.id.coin_view_dime,
+            R.id.coin_view_nickel,
+            R.id.coin_view_penny
+    })
+    public void onClick(CoinView view) {
+
     }
 }
--- app/src/main/java/com/ariets/kata/ui/widget/CoinView.java
+++ app/src/main/java/com/ariets/kata/ui/widget/CoinView.java
@@ -1,6 +1,7 @@
 package com.ariets.kata.ui.widget;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.support.annotation.ColorInt;
@@ -10,7 +11,11 @@ import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,6 +28,7 @@ import butterknife.ButterKnife;
 
 import static android.graphics.Color.BLACK;
 import static android.graphics.Color.WHITE;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 import static com.ariets.kata.model.Coin.DIME;
 import static com.ariets.kata.model.Coin.NICKEL;
 import static com.ariets.kata.model.Coin.PENNY;
@@ -58,16 +64,23 @@ public class CoinView extends FrameLayout {
         LayoutInflater.from(context).inflate(R.layout.view_coin, this, true);
         ButterKnife.bind(this);
 
+        setupLayoutParams();
+
         if (attrs == null) {
             return;
         }
-        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CoinView, 0, 0);
+        Resources.Theme theme = context.getTheme();
+        TypedArray typedArray = theme.obtainStyledAttributes(attrs, R.styleable.CoinView, 0, 0);
         try {
             int coinType = typedArray.getInteger(R.styleable.CoinView_coinType, 0);
             if (coinType > 0) {
                 setCoinType(coinType);
             }
             setupView();
+
+            TypedValue outValue = new TypedValue();
+            getContext().getTheme().resolveAttribute(android.R.attr.selectableItemBackground, outValue, true);
+            setBackgroundResource(outValue.resourceId);
         } finally {
             typedArray.recycle();
         }
@@ -87,6 +100,12 @@ public class CoinView extends FrameLayout {
             default:
                 coin = PENNY;
         }
+    }
+
+    private void setupLayoutParams() {
+        FrameLayout.LayoutParams params = new LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+        params.gravity = Gravity.CENTER;
+        setLayoutParams(params);
     }
 
     private void setupView() {
@@ -117,5 +136,10 @@ public class CoinView extends FrameLayout {
         imageView.setColorFilter(ContextCompat.getColor(getContext(), tintColor));
         textView.setText(textRes);
         textView.setTextColor(textColor);
+    }
+
+    @Nullable
+    public Coin getCoin() {
+        return coin;
     }
 }
